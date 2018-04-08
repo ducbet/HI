b5490ed1677d07dd82e1a6a558dc24c77aac3fdd
ducbet <ducbet1996@gmail.com>
2018-04-08 20:35:15 +0700

    quick choose

--- app/src/main/java/com/example/tmd/hi_20172/model/StopOver.java
+++ app/src/main/java/com/example/tmd/hi_20172/model/StopOver.java
@@ -1,17 +1,46 @@
 package com.example.tmd.hi_20172.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by tmd on 07/04/2018.
  */
 
-public abstract class StopOver {
+public class StopOver implements Parcelable {
     protected String id = "";
     protected LatLng latlon;
     protected int icon;
     protected boolean choose;
     protected String name;
+
+    public StopOver(LatLng latlon, int icon, String name) {
+        this.latlon = latlon;
+        this.icon = icon;
+        this.name = name;
+    }
+
+    protected StopOver(Parcel in) {
+        id = in.readString();
+        latlon = in.readParcelable(LatLng.class.getClassLoader());
+        icon = in.readInt();
+        choose = in.readByte() != 0;
+        name = in.readString();
+    }
+
+    public static final Creator<StopOver> CREATOR = new Creator<StopOver>() {
+        @Override
+        public StopOver createFromParcel(Parcel in) {
+            return new StopOver(in);
+        }
+
+        @Override
+        public StopOver[] newArray(int size) {
+            return new StopOver[size];
+        }
+    };
 
     public String getId() {
         return id;
@@ -51,5 +80,19 @@ public abstract class StopOver {
 
     public void setChoose(boolean choose) {
         this.choose = choose;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(id);
+        parcel.writeParcelable(latlon, i);
+        parcel.writeInt(icon);
+        parcel.writeByte((byte) (choose ? 1 : 0));
+        parcel.writeString(name);
     }
 }
